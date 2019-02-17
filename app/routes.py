a1cfe84b3be26f960c61bac30d4d7cbcbd03a495
Chris Brown <chris@cfbrown.co.uk>
2019-02-17 17:54:09 +0000

    First content

--- app/routes.py
+++ app/routes.py
@@ -8,12 +8,17 @@ def index():
     user = {'username': 'Chris'}
     posts = [
         {
-            'author': {'username': 'Emily'},
-            'body': 'Beautiful day in Oxford!'
+            'author': {'username': 'Chris'},
+            'body': """
+Decided that I wanted to try and do more blogging about stuff that I learn, so I though what better way to start than by
+making the blog site from scratch in python to learn some web development in the process. Plus 
+https://airflow.apache.org/Airflow uses Flask for it's UI I believe, so it'll be a good learning
+experience for that to.
+"""
         },
         {
-            'author': {'username': 'Alex'},
-            'body': 'The new Star Trek is so cool!'
+            'author': {'username': 'Chris'},
+            'body': 'Hopefully I actually make more blog posts...'
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
