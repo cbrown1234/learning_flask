8702b255545e7238635a7897c4e12c0b7000a374
Chris Brown <chris@cfbrown.co.uk>
2019-02-17 17:36:58 +0000

    Basic Jinja Templating

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,20 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World!'
+    user = {'username': 'Chris'}
+    posts = [
+        {
+            'author': {'username': 'Emily'},
+            'body': 'Beautiful day in Oxford!'
+        },
+        {
+            'author': {'username': 'Alex'},
+            'body': 'The new Star Trek is so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
